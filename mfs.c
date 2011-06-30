2cff6c58a7c444185bfac7d5041ba582dd7ab29a
Nathanael Barry <hominemcura@gmail.com>
2011-06-30 14:24:07 +0100

    Works

--- mfs.c
+++ mfs.c
@@ -1,9 +1,9 @@
-/*********************************/
-/* Mfs				 */
-/* Don't expect this to work     */
-/* Licenced under the MIT licence*/
-/* Which is included by the way  */
-/*********************************/
+/*********************************************/
+/* Mfs				             */
+/* Don't expect this to work                 */
+/* May not be sold without written permission*/
+/* (c) 2011 Nathanael barry     	     */
+/*********************************************/
 
 #include <stdio.h>
 
@@ -19,6 +19,8 @@ int main(int argc, char *argv[]){
 	int cyphered;
 	int c; //character storage
 	FILE * inFile;
+	FILE * outFile;
+	int outstat;
 	FILE * helpFile;
 	int rotnum = 13; //rot num
 
@@ -48,19 +50,35 @@ int main(int argc, char *argv[]){
 					}else{
 						rotnum = atoi(argv[2]);
 					}
+					if ( argv[3] == NULL ){
+						outstat = 0;
+					}else{
+						outstat = 1;
+					}						
 					inFile=fopen (argv[1],"r");
 					if (inFile==NULL){
 						perror ("Error opening file");
 					}
-					while(c != EOF && c != NULL){
-						c = fgetc(inFile);
-						cyphered = rot(rotnum, c);
-						putchar(cyphered);
+					if ( outstat == 0 ){
+						while(c != EOF){
+							c = fgetc(inFile);
+							cyphered = rot(rotnum, c);
+							putchar(cyphered);
+						}
+						printf("\n");
+						fclose (inFile);
+					}else{
+						outFile=fopen (argv[3], "w");
+						while(c != EOF){
+							c = fgetc(inFile);	
+							cyphered = rot(rotnum, c);
+							fputc(c, outFile);	
+						}
+						fclose (inFile);
+						fclose (outFile);								
 					}
-					fclose (inFile);
-					}
-	}
-	printf("\n");
+			}
+	}	
 	return 0;
 
 }
